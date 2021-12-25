99b839a24a0ee08f9a5dbfb17993300deda633f9
pinchocrates <ruby@onconsultants.com>
2021-12-25 14:37:11 -0500

    not working - - route issues

--- myapp.rb
+++ myapp.rb
@@ -7,12 +7,21 @@ class Myapp < Roda
     plugin :head
   
     route do |r|
-      r.root do
-        ### @title = "... and note: This variable is visible by the page called by view/render"
-        @layout_str = "Layout String"
-        @homepage_str = "Homepage String"
-        view("homepage") # this here uses the render plugin
-        ### render("homepage") # this here uses the render plugin, notbut does not use the layout
-      end
+        r.root do
+            ### @title = "... and note: This variable is visible by the page called by view/render"
+            @layout_str = "Layout String"
+            @homepage_str = "Homepage String"
+            view("homepage") # this here uses the render plugin
+            ### render("homepage") # this here uses the render plugin, notbut does not use the layout
+        end
+
+        r.get "about" do
+            view("about")
+        end
+        
+        r.get "contact" do
+            view("contact")
+        end
+
     end
-  end
+end
